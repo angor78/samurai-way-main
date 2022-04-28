17ba701b585f2c2f554086b186be35c3bb925983
Angor78 <angor78@gmail.com>
2022-04-28 09:41:14 +0300

    SocialNet:html,css grid + logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import logo from './images/logo.png';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app-wraper">
+      <header className="header">
+        <img src={logo} width={100}
+             alt="logo"/>
+      </header>
+      <nav className="nav">
+        <div><a href='#'>Profile</a></div>
+        <div><a href='#'>Messages</a></div>
+        <div><a href='#'>News</a></div>
+        <div><a href='#'>Musik</a></div>
+        <div><a href='#'>Settings</a></div>
+      </nav>
+      <div className="content">
+        <div>Ava+description</div>
+        <div>
+          MY POSTS
+          <div>new post
+          </div>
+          <div>1 post</div>
+          <div>2 post</div>
+        </div>
+
+      </div>
     </div>
   );
 }
