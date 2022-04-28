e26653a106145d13865847b10b18869e0cf0a830
Angor78 <angor78@gmail.com>
2022-04-28 13:22:20 +0300

    FolderStruct newComponent refact(L15)

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Profile.css"
+
+
+const Profile = () => {
+  return (<div className="content">
+    <div>Ava+description</div>
+    <div className="posts">
+      MY POSTS
+      <div>new post</div>
+      <div>1 post</div>
+      <div>2 post</div>
+    </div>
+
+  </div>)
+}
+
+export default Profile
