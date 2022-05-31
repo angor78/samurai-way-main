c1fa6244104fa0afcf7452f63314847db79d6f21
Angor78 <angor78@gmail.com>
2022-05-31 12:28:05 +0300

    delete Friends

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from "./Navbar.module.css"
 import {NavLink} from "react-router-dom";
-import {Friends} from "../Friends/Friends";
 
 const Navbar = () => {
   return (
