dc12109489687f3da64b8dc98d4d75f776a86e85
Angor78 <angor78@gmail.com>
2022-05-31 12:28:27 +0300

    Profile refactor with dispatch and reducer

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,25 +2,26 @@ import React from "react";
 import "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {ProfilePageType} from "../../redux/state";
-
+import {
+  ActionTypes,
+  ProfilePageType
+} from "../../redux/state";
 
 
 export type ProfilePropsType = {
-  profilePage:ProfilePageType
-  addPost: (textToPost:string) => void
-  changeTextPost:(postText:string)=>void
-  newPostText:string
+  profilePage: ProfilePageType
+  dispatch: (action: ActionTypes) => void
+  newPostText: string
 }
 const Profile = (props: ProfilePropsType) => {
 
   return (
     <div className="content">
-     <ProfileInfo/>
+      <ProfileInfo/>
       <MyPosts posts={props.profilePage.posts}
-               addPost={props.addPost}
-               changeTextPost={props.changeTextPost}
-               newPostText={props.newPostText}/>
+               dispatch={props.dispatch}
+               newPostText={props.newPostText}
+      />
     </div>
   )
 
