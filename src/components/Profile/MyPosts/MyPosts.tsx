e8b41e180cf9cf002b8223fe4a2446b667a0b85f
Angor78 <angor78@gmail.com>
2022-06-07 09:37:15 +0300

    fix import to MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,7 +4,7 @@ import s from "./MyPosts.module.css"
 import {
   ActionTypes,
   PostType
-} from "../../../redux/state";
+} from "../../../redux/store";
 import {addPostActionCreator, changeTextPostActionCreator} from "../../../redux/profile-reducer";
 
 
