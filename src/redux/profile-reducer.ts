6d09424330b9b539930abd88a4d810fad8a2527b
Angor78 <angor78@gmail.com>
2022-06-13 19:37:46 +0300

    refactor reducer and AC in profile-reducer

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,46 +1,47 @@
 import {v1} from "uuid";
-import {ActionTypes} from "./store";
+import {ActionTypes, PostType} from "./storeTypes";
 
 
 const ADD_POST = 'ADD-POST'
 const CHANGE_TEXT_POST = 'CHANGE-TEXT-POST'
 
-type initialStateType = {
+export type initialProfileStateType = {
   newTextPost: string
   posts: Array<{id: string, message: string, likeCount: number}>
 
 }
 
-let initialState:initialStateType = {
+let initialState = {
   newTextPost: '',
   posts: [
-    {id: v1(), message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad assumenda at consequatur cumque dolore eius est ipsam iure magnam magni, nihil placeat quia quibusdam, quis, temporibus voluptatem. Accusantium, quos?\n', likeCount: 12},
     {id: v1(), message: "It's my first yo.", likeCount: 1},
     {id: v1(), message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", likeCount: 1},
     {id: v1(), message: "It's my first yo.", likeCount: 1},
-  ]
+    {id: v1(), message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad assumenda at consequatur cumque dolore eius est ipsam iure magnam magni, nihil placeat quia quibusdam, quis, temporibus voluptatem. Accusantium, quos?\n', likeCount: 12},
+
+  ]as Array<PostType>
 }
 
 
 
-export const profileReducer = (state = initialState, action:ActionTypes):initialStateType => {
+export const profileReducer = (state:initialProfileStateType = initialState, action:ActionTypes):initialProfileStateType => {
   switch (action.type) {
     case ADD_POST:
-      let newPost = {id: v1(), message: action.newTextPost, likeCount: 0}
+      let newPost = {id: v1(), message: state.newTextPost, likeCount: 0}
       let newPosts = [newPost,...state.posts]
       state.newTextPost = ''
       return {...state,posts:newPosts}
     case CHANGE_TEXT_POST:
       state.newTextPost = action.postText
-      return state
+      return {...state}
     default:
       return state
   }
 }
 
 
-export const addPostActionCreator = (newPostText: string) => {
-  return {type: 'ADD-POST', newTextPost: newPostText} as const
+export const addPostActionCreator = () => {
+  return {type: 'ADD-POST'} as const
 }
 export const changeTextPostActionCreator = (postText: string) => {
   return {type: 'CHANGE-TEXT-POST', postText: postText} as const
