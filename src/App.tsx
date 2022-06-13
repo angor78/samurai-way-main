63fc82b8d60956848cd7369e13f834c41976857b
Angor78 <angor78@gmail.com>
2022-06-13 20:06:39 +0300

    refactor with container components

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,34 @@
 import React from "react";
-import { ChakraProvider} from '@chakra-ui/react'
-import Profile from "./components/Profile/Profile";
+import {ChakraProvider} from '@chakra-ui/react'
 import "./App.css"
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {StoreType} from "./redux/store";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import {Route, Routes} from "react-router-dom";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import HeaderWithAction from "./components/HeaderWithActionType/HeaderWithActionType";
 import {MainImage} from "./components/MainImage/MainImage";
+import {ProfileContainer} from "./components/Profile/ProfileContainer";
 
-
-type AppPropsType = {
-  store: StoreType
-}
-
-function App(props: AppPropsType) {
+function App() {
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <HeaderWithAction/>
-        <div className="app-wrap">
-          <div className="app-wrap-content">
-            <Routes>
-              <Route path="/" element={<MainImage/>}/>
-              <Route path="/main" element={<MainImage/>}/>
-              <Route path={"/dialogs"}
-                     element={<DialogsContainer
-                       store={props.store}
-                     />}/>
-              <Route path={"/profile"}
-                     element={<Profile
-                       store={props.store}
-                     />}/>
-
-              <Route path={"/music"} element={<Music/>}/>
-              <Route path={"/news"} element={<News/>}/>
-              <Route path={"/settings"} element={<Settings/>}/>
-            </Routes>
-          </div>
+      <HeaderWithAction/>
+      <div className="app-wrap">
+        <div className="app-wrap-content">
+          <Routes>
+            <Route path="/" element={<MainImage/>}/>
+            <Route path="/main" element={<MainImage/>}/>
+            <Route path={"/dialogs"}
+                   element={<DialogsContainer/>}/>
+            <Route path={"/profile"}
+                   element={<ProfileContainer/>}/>
+            <Route path={"/music"} element={<Music/>}/>
+            <Route path={"/news"} element={<News/>}/>
+            <Route path={"/settings"} element={<Settings/>}/>
+          </Routes>
         </div>
-      </BrowserRouter>
+      </div>
     </ChakraProvider>
   );
 }
