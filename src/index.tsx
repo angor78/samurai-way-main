96cf600a68d2f8cce46a15298af118489e4605bd
Angor78 <angor78@gmail.com>
2022-06-13 20:05:01 +0300

    add Provider react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,19 @@ import './index.css';
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
 
 let renderTree = () => {
   ReactDOM.render(
-      <App store={store}
-  />,
-  document.getElementById('root')
-)
+    <BrowserRouter>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </BrowserRouter>
+    ,
+    document.getElementById('root')
+  )
   ;
 }
 
