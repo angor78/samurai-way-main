1f938caf688f8191e769a98562e9984acd82c808
Angor78 <angor78@gmail.com>
2022-07-06 21:31:22 +0300

    add auth reducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,11 +3,13 @@ import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {ActionTypes, StoreType} from "./storeTypes";
 import {usersReducer} from "./users-reducer";
+import {authReducer} from "./auth-reducer";
 
 let rootReducer = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
-  usersPage: usersReducer
+  usersPage: usersReducer,
+  auth: authReducer,
 })
 export type AppStateType = ReturnType<typeof rootReducer>
 
