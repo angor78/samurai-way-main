ffb22ae3701bd543ab3e81826f6c0e8bf3830cab
Angor78 <angor78@gmail.com>
2022-07-25 14:57:16 +0300

    refactor dialogs

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import {Box} from "@chakra-ui/react";
 import {DialogType} from "../../../redux/storeTypes";
+import logo from './../../../images/logo.png'
 
 
 export const DialogItem = (props: DialogType) => {
   //let path = "/dialogs/" + props.id
   return (
 
-    <Box bg='blue.300' w='100%' p={4} color='white'>
+    <Box bg='blue.300' w='100%' p={4} pl={'0'} color='white' display={'flex'} flexDirection={'row'}>
       {/*<NavLink to={path} style={isActive => ({*/}
       {/*  color: isActive ? "teal" : "white",*/}
       {/*  textDecoration: "none"*/}
       {/*})}>*/}
 
-        <Box>
-            {props.name}
+        <Box >
+          <img width={'50px'} height={'50px'} src={logo} alt="logo"/>
         </Box>
+      <Box >
+        {props.name}
+      </Box>
 
       {/*</NavLink>*/}
     </Box>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,16 @@
-import React, {ChangeEvent} from "react";
+import React from "react";
 import s from "./Dialogs.module.css"
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {Box, Button, Input} from "@chakra-ui/react";
 import {DialogsPropsType} from "./DialogsContainer";
-
+import {FormikProps, withFormik} from "formik";
 
 
 const Dialogs = (props: DialogsPropsType) => {
 
-  let messagesElements = props.dialogsPage.messagesData.map((m,i) =>
-      i % 2 === 0 ?
+  let messagesElements = props.dialogsPage.messagesData.map((m, i) =>
+    i % 2 === 0 ?
       <Box key={m.id} textAlign={'left'}>{<Message message={m.message} id={m.id}/>}</Box>
       : <Box key={m.id} textAlign={'right'}>{<Message message={m.message} id={m.id}/>}</Box>
   )
@@ -21,29 +21,79 @@ const Dialogs = (props: DialogsPropsType) => {
     </Box>
   )
 
-  const onClickAddMessageHandler = () => {
-    props.addMessage()
+  const addNewMessage = (value: string) => {
+    props.addMessage(value)
   }
-  const onChangeMessageHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    props.changeMessageText(e.currentTarget.value)
-  }
+  // const onChangeMessageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  //   props.changeMessageText(e.currentTarget.value)
+  // }
 
   return (
-    <Box display={'flex'}  className={s.dialogs}  >
-      <Box display={'block'}  >
+    <Box display={'flex'} className={s.dialogs}>
+      <Box display={'block'}>
         {dialogsElements}
       </Box>
       <Box>
         {messagesElements}
-        <Box>
-          <Input placeholder='New message...' onChange={onChangeMessageHandler}
-                    value={props.dialogsPage.newTextMessage} color={'teal'}/>
-          <Button colorScheme='teal' mt={'5'} mb={'15'} size='sm' onClick={onClickAddMessageHandler}>
-            add message
-          </Button>
-        </Box>
+        <AddMessageForm addNewMessage={addNewMessage}/>
       </Box>
     </Box>
   )
 }
 export default Dialogs
+
+
+interface FormValues {
+  newTextMessage: string
+}
+
+interface OtherProps {
+  title?: string;
+}
+
+interface MyFormProps {
+  initialNewTextMessage?: string;
+  addNewMessage: (value: string) => void
+}
+
+const AddMessageInnerForm = (props: OtherProps & FormikProps<FormValues>) => {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    title
+  } = props;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input placeholder='New message...'
+             onChange={handleChange}
+             type={'text'}
+             name={'newTextMessage'}
+             value={values.newTextMessage}
+             color={'teal'}/>
+      <Button type="submit"
+              colorScheme='teal'
+              mt={'5'}
+              mb={'15'}
+              size='sm'>
+        add message
+      </Button>
+    </form>
+  )
+}
+
+
+export const AddMessageForm = withFormik<MyFormProps, FormValues>({
+  mapPropsToValues: props => ({
+    newTextMessage: props.initialNewTextMessage || "",
+  }),
+  handleSubmit({newTextMessage}: FormValues, {props, setSubmitting, setErrors}) {
+    props.addNewMessage(newTextMessage)
+    setSubmitting(false)
+  }
+})(AddMessageInnerForm)
